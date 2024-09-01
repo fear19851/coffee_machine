6f0797c21f3af96908d9b5a1dc34629f6544aa6a
fear19851 <fearnokappa@gmail.com>
2024-09-02 02:36:11 +0500

    completed

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-'''
-bafkbnaosbnfoasbnfaosklnloafksnadfklsnkl;afsmnkl;afsnkl;afsnmlp;fas
-aflsmpl;afmslfasm
-afksnmafksklp;
-fsaafsfasfasasasasasasasasasasasasasasasasasas
-'''
+money_machine = MoneyMachine()
+coffee_maker=CoffeeMaker()
+menu=Menu()
+is_on=True
+
+
+while is_on:
+    options=menu.get_items()
+    choice=input(f"what would u like ({options}): ")
+    if choice=="off":
+        is_on=False
+    elif choice=="report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choice)
+        print(drink)
+        if coffee_maker.is_resource_sufficient(drink) and  money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
