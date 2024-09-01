158fa3bf85cbe105ec188a4f1df84f0c3c640043
fear19851 <fearnokappa@gmail.com>
2024-09-01 20:04:56 +0500

    change in comments in  main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+'''
+bafkbnaosbnfoasbnfaosklnloafksnadfklsnkl;afsmnkl;afsnkl;afsnmlp;fas
+aflsmpl;afmslfasm
+afksnmafksklp;
+
+'''
